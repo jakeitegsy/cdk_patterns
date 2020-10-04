2ed78dee1dd05de25c96cec7133680a66c1da625
Jake Itegboje <jakeitegsy@yahoo.com>
2020-10-04 12:15:13 -0500

    Create 00_setup_aws_cdk_mac_os.sh

---
+++ 00_setup_aws_cdk_mac_os.sh
@@ -0,0 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install node
+npm install -g aws-cdk
+sudo ln -s /folder/installed/aws-cli/aws /usr/local/bin/aws
+sudo ln -s /folder/installed/aws-cli/aws_completer /usr/local/bin/aws/aws_completer
