f4d5b259fc12b88c6428fb168a3a8d8941dcd274
Jake Itegboje <jakeitegsy@yahoo.com>
2020-10-04 12:19:29 -0500

    Update 00_setup_aws_cdk_mac_os.sh with Python3 install steps

--- 00_setup_aws_cdk_mac_os.sh
+++ 00_setup_aws_cdk_mac_os.sh
@@ -3,3 +3,6 @@ brew install node
 npm install -g aws-cdk
 sudo ln -s /folder/installed/aws-cli/aws /usr/local/bin/aws
 sudo ln -s /folder/installed/aws-cli/aws_completer /usr/local/bin/aws/aws_completer
+curl https://www.python.org/ftp/python/3.8.6/python-3.8.6-macosx10.9.pkg
+sudo installer -pkg python-3.8.6-macosx10.9.pkg
+python3 -ensure pip
