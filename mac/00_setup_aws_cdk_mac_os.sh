0c9131d317ae62a108a48f85c2beea01693e2547
Jake Itegboje <jakeitegsy@yahoo.com>
2020-10-10 23:09:48 +0000

    correct AWSCLIV2 installer

--- mac/00_setup_aws_cdk_mac_os.sh
+++ mac/00_setup_aws_cdk_mac_os.sh
@@ -1,8 +1,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install node
 npm install -g aws-cdk
-sudo ln -s /folder/installed/aws-cli/aws /usr/local/bin/aws
-sudo ln -s /folder/installed/aws-cli/aws_completer /usr/local/bin/aws/aws_completer
-curl https://www.python.org/ftp/python/3.8.6/python-3.8.6-macosx10.9.pkg
-sudo installer -pkg python-3.8.6-macosx10.9.pkg
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+curl https://www.python.org/ftp/python/3.9.0/python-3.9.0-macosx10.9.pkg -o "python.pkg"
+sudo installer -pkg python.org
 python3 -ensure pip
