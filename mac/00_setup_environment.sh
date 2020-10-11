01ff312c978b46082619ce027a1b3cf81cd74287
Jake Itegboje <jakeitegsy@yahoo.com>
2020-10-11 00:02:04 +0000

    Update and rename 00_setup_aws_cdk_mac_os.sh to 00_setup_environment.sh

--- mac/00_setup_environment.sh
+++ mac/00_setup_environment.sh
@@ -1,8 +1,9 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 curl "https://nodejs.org/dist/v12.19.0/node-v12.19.0.pkg" -o "node.pkg"
 sudo installer -pkg node.pkg -target /
-npm install -g aws-cdk
+sudo npm install -g aws-cdk
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 curl https://www.python.org/ftp/python/3.9.0/python-3.9.0-macosx10.9.pkg -o "python.pkg"
 sudo installer -pkg python.org -target /
-python3 -ensure pip
+python3 -m ensurepip
