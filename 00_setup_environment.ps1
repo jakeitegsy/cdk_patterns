19b0347415731abfb33e45869486df3d971170e2
Jake Itegboje <jakeitegsy@yahoo.com>
2020-10-18 12:07:08 -0500

    Create 00_setup_environment.ps1

---
+++ 00_setup_environment.ps1
@@ -0,0 +1,26 @@
+#Setup Environment
+$nodeVersion = "v14.14.0"
+$nodePackage = "node-$nodeVersion-x64.msi"
+
+$awsCliPackage = "AWSCLIV2.msi"
+
+$pythonVersion = "3.9.0"
+$pythonPackage = "python-$pythonVersion-amd64.exe"
+
+
+$packages = @{
+    "node.msi"="https://nodejs.org/dist/$nodeVersion/$nodePackage";
+    $awsCliPackage="https://awscli.amazonaws.com/$awsCliPackage";
+    "python.exe"="https://www.python.org/ftp/python/$pythonVersion/$pythonPackage"
+}
+
+$destination = "$Env:UserProfile\Downloads"
+
+npm install -g aws-cdk
+
+cd $destination
+forEach ($package in $packages.keys) {
+    (New-Object System.Net.WebClient).DownloadFile($packages.$package, "$destination\$package")
+    Start-Process $package -Wait
+    Remove-Item $destination\$package -Force
+}
